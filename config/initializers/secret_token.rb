89c88d880cc2c3b3f44d3eb2d2397763f3d91579
Daniel Kaplan <daniel.scott.kaplan@gmail.com>
2012-07-19 19:58:54 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DerpSansa::Application.config.secret_token = '65788c11419c549f607024f016b652d748bb86de099b251b109f52f531ef625118b49256f9594cb3e41cd4c6ec0f877f7127f8c861c723a9779124b61ebac68b'
